5e5a1f6670df724cfce287474623493bc742ec63
Sarisia <sarisiaice@gmail.com>
2022-09-03 20:50:14 +0900

    fish: fix virtualenv config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,8 +8,8 @@ set color_status_superuser_bg ffffff
 set color_status_superuser_str 000000
 set color_status_jobs_bg ffffff
 set color_status_jobs_str 000000
-set color_virtual_env_bg 
-set color_virtual_env_str
+set color_virtual_env_bg 4ac0ff
+set color_virtual_env_str 000000
 set color_user_bg ff96b6
 set color_user_str 000000
 set color_dir_bg 000000
