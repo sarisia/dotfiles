fcf1f30e6e511105d22b560ff7cded3594810bc3
sarisia <git@sarisia.cc>
2024-05-13 15:28:34 +0900

    fix gg

--- .config/fish/functions/gg.fish
+++ .config/fish/functions/gg.fish
@@ -1,3 +1,3 @@
 function gg --wraps='gh copilot suggest -t shell' --description 'alias gg gh copilot suggest -t shell'
-  gh copilot suggest -t shell $argv
+  gh copilot suggest -t shell "$argv"
 end
